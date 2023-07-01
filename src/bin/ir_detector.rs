823c1c7a02c169745eddd49bf19e998f9c1e5199
aaron-hardin <aaronrhardin@gmail.com>
2023-07-01 19:34:52 +0900

    Update ir_detector to work with my hardware -- detector needs 38khz flashing to trigger

--- src/bin/ir_detector.rs
+++ src/bin/ir_detector.rs
@@ -4,7 +4,7 @@
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::{InputPin, OutputPin};
+use embedded_hal::{digital::v2::{InputPin, OutputPin}, PwmPin};
 use panic_probe as _;
 
 use rp_pico as bsp;
@@ -48,10 +48,30 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
+    let sys_freq = clocks.system_clock.freq().to_Hz();
+
     let mut status_led = pins.led.into_push_pull_output();
     status_led.set_high().unwrap();
     let detector_in_pin = pins.gpio18.into_floating_input();
     let boot_mode_in_pin = pins.gpio27.into_floating_input();
+
+    // Init PWMs
+    let mut pwm_slices = bsp::hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+
+    // Configure PWM2 --  38 khz
+    let pwm_ir_led = &mut pwm_slices.pwm2;
+    pwm_ir_led.default_config();
+    pwm_ir_led.set_ph_correct();
+    pwm_ir_led.set_div_int(1);
+    let top: u16 = (sys_freq / 38_000 / 2).try_into().unwrap();
+    let duty = top / 2;
+    pwm_ir_led.set_top(top);
+    pwm_ir_led.enable();
+    // Output channel A on PWM2 to the GPIO4 pin
+    let channel_ir_led = &mut pwm_ir_led.channel_a;
+    channel_ir_led.output_to(pins.gpio4);
+    // Set the duty cycle to 50%
+    channel_ir_led.set_duty(duty);
 
     loop {
         if boot_mode_in_pin.is_high().unwrap() {
